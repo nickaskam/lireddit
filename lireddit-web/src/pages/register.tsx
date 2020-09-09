c92b7d2d8587e104ce971a7183b8455592673073
Nick Askam <asknickfilms@gmail.com>
2020-09-09 12:32:26 -0500

    adding pages and converting to typescript

---
+++ lireddit-web/src/pages/register.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface registerProps {}
+
+export const Register: React.FC<registerProps> = ({}) => {
+  return <div>Register page</div>;
+};
+
+export default Register;
