d536f6f441163e13b4295bb24b7077917a6b437a
Nick Askam <asknickfilms@gmail.com>
2020-09-09 15:05:37 -0500

    adding in cors to the server

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -4,16 +4,33 @@ import { Formik, Form } from "formik";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { Box, Button } from "@chakra-ui/core";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
+const REGISTER_MUT = `
+mutation Register($username:String!, $password:String!) {
+  register(options: { username: $username, password: $password }) {
+    errors {
+      field 
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}`;
+
 export const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUT);
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={(values) => {
           console.log(values);
+          return register(values);
         }}
       >
         {({ isSubmitting }) => (
