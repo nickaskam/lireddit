51004ae30c413962ee7792df3a13a988205f7912
Nick Askam <asknickfilms@gmail.com>
2020-09-09 12:46:04 -0500

    adding in the wrapper and username form element

--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { Formik, Form } from "formik";
+import { FormControl, FormLabel, Input } from "@chakra-ui/core";
+import { Wrapper } from "../components/Wrapper";
 
 interface registerProps {}
 
 export const Register: React.FC<registerProps> = ({}) => {
-  return <div>Register page</div>;
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ values, handleChange }) => (
+          <Form>
+            <FormControl>
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input
+                value={values.username}
+                onChange={handleChange}
+                id="username"
+                placeholder="username"
+              />
+              {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
+            </FormControl>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
 
 export default Register;
