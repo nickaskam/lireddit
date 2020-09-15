a7f0cc78ffe3eb6a70ee5257f3e2524a8ecb2bb9
Nick Askam <asknickfilms@gmail.com>
2020-09-14 23:13:23 -0500

    adding the logout function to the front end

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -12,7 +12,7 @@ import { MyContext } from "../types";
 import { User } from "../entities/User";
 import argon2 from "argon2";
 import { EntityManager } from "@mikro-orm/postgresql";
-import { COOKIE_NAME } from "src/constants";
+import { COOKIE_NAME } from "../constants";
 
 @InputType()
 class UsernamePasswordInput {
