7150ac95d6023530c29527b2e10628ba2faf55e3
Nick Askam <asknickfilms@gmail.com>
2020-09-17 14:31:08 -0500

    adding in the page to create post and move you to the login page if you're not logged in

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,17 +1,21 @@
-import { NavBar } from "../components/NavBar";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
+import { Layout } from "../components/Layout";
+import { Link } from "@chakra-ui/core";
+import NextLink from "next/link";
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
   return (
-    <>
-      <NavBar />
+    <Layout>
+      <NextLink href="/create-post">
+        <Link>create post</Link>
+      </NextLink>
       <div>hello world</div>
       <br />
       {!data ? null : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
-    </>
+    </Layout>
   );
 };
 
