70ffea3f941c8ee72233a8c26965f08b9ec95979
Nick Askam <asknickfilms@gmail.com>
2020-09-18 16:58:41 -0500

    set up relationships for voting and give user ability to vote

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -16,6 +16,7 @@ import {
 } from "type-graphql";
 import { Post } from "../entities/Post";
 import { getConnection } from "typeorm";
+import { Updoot } from "../entities/Updoot";
 
 @InputType()
 class PostInput {
@@ -38,6 +39,30 @@ export class PostResolver {
   @FieldResolver(() => String)
   textSnippet(@Root() root: Post) {
     return root.text.slice(0, 50);
+  }
+
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuth)
+  async vote(
+    @Arg("postId", () => Int) postId: number,
+    @Arg("value", () => Int) value: number,
+    @Ctx() { req }: MyContext
+  ) {
+    const isUpdoot = value !== -1;
+    const realValue = isUpdoot ? 1 : -1;
+    const { userId } = req.session;
+    await getConnection().query(
+      `
+      START TRANSACTION;
+      INSERT INTO updoot ("userId", "postId", value)
+      values (${userId},${postId},${realValue});
+      UPDATE post
+      SET points = points + ${realValue}
+      WHERE id = ${postId};
+      COMMIT;
+      `
+    );
+    return true;
   }
 
   @Query(() => PaginatedPosts)
