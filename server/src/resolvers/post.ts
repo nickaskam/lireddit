71f782097324be83cd5cd22cbac9f2d8e5491951
Nick Askam <asknickfilms@gmail.com>
2020-09-23 16:57:22 -0500

    fixing import error and mapping error

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -44,7 +44,7 @@ export class PostResolver {
 
   @FieldResolver(() => User)
   creator(@Root() post: Post, @Ctx() { userLoader }: MyContext) {
-    return userLoader.load(post.id);
+    return userLoader.load(post.creatorId);
   }
 
   @Mutation(() => Boolean)
--- server/src/utils/createUserLoader.ts
+++ server/src/utils/createUserLoader.ts
@@ -1,5 +1,5 @@
 import DataLoader from "dataloader";
-import { User } from "src/entities/User";
+import { User } from "../entities/User";
 
 // [1, 7, 8, 9]
 // [{id: 1, username: 'tim'}, {id: 7, username: 'bob'}, {}, {}]
